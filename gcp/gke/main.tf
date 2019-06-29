44885be3a219d353aa0dbcdc29dd3bb511f99729
kaysal <salawu@google.com>
2019-06-29 20:20:09 +0100

    upgraded to terraform 0.12

--- gcp/gke/main.tf
+++ gcp/gke/main.tf
@@ -1,85 +1,108 @@
-
 resource "google_container_cluster" "cluster" {
-  provider                          = "google-beta"
-  project                           = "${var.project_id}"
-  name                              = "${var.name}"
-  min_master_version                = "${var.min_master_version}"
-  network                           = "${var.network}"
-  subnetwork                        = "${var.subnetwork}"
-  location                          = "${var.location}"
-  default_max_pods_per_node         = "${var.default_max_pods_per_node}"
-  remove_default_node_pool          = "${var.remove_default_node_pool}"
-  logging_service                   = "${var.logging_service}"
-  monitoring_service                = "${var.monitoring_service}"
-  enable_binary_authorization       = "${var.enable_binary_authorization}"
-  initial_node_count                = "${var.node_count}"
-  resource_labels                   = "${var.cluster_labels}"
-  master_authorized_networks_config = "${var.master_authorized_networks_config}"
+  provider = google-beta
+  project  = var.project
 
-  private_cluster_config {
-    enable_private_endpoint = "${var.enable_private_endpoint}"
-    enable_private_nodes    = "${var.enable_private_nodes}"
-    master_ipv4_cidr_block  = "${var.master_ipv4_cidr_block}"
-  }
+  name            = var.name
+  description     = var.description
+  resource_labels = var.cluster_labels
+  location        = var.location
+  network         = var.network
+  subnetwork      = var.subnetwork
+
+  logging_service    = var.logging_service
+  monitoring_service = var.monitoring_service
+  min_master_version = local.kubernetes_version
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  default_max_pods_per_node   = var.default_max_pods_per_node
+  enable_binary_authorization = var.enable_binary_authorization
 
   ip_allocation_policy {
-    cluster_secondary_range_name  = "${var.pods_range_name}"
-    services_secondary_range_name = "${var.services_range_name}"
+    cluster_secondary_range_name  = var.pods_range_name
+    services_secondary_range_name = var.services_range_name
+  }
+
+  private_cluster_config {
+    enable_private_endpoint = var.disable_public_endpoint
+    enable_private_nodes    = var.enable_private_nodes
+    master_ipv4_cidr_block  = var.master_ipv4_cidr_block
+  }
+
+  dynamic "master_authorized_networks_config" {
+    for_each = var.master_authorized_networks_config
+    content {
+      dynamic "cidr_blocks" {
+        for_each = lookup(master_authorized_networks_config.value, "cidr_blocks", [])
+        content {
+          cidr_block   = cidr_blocks.value.cidr_block
+          display_name = lookup(cidr_blocks.value, "display_name", null)
+        }
+      }
+    }
   }
 
   addons_config {
-    network_policy_config {
-      disabled = "${var.network_policy_config_disabled}"
+    http_load_balancing {
+      disabled = ! var.http_load_balancing
+    }
+
+    horizontal_pod_autoscaling {
+      disabled = ! var.horizontal_pod_autoscaling
     }
 
     kubernetes_dashboard {
-      disabled = "${var.kubernetes_dashboard_disabled}"
+      disabled = ! var.enable_kubernetes_dashboard
     }
 
+    network_policy_config {
+      disabled = ! var.enable_network_policy
+    }
+    /*
     istio_config {
-      disabled = "${var.istio_config_disabled}"
-
+      disabled = ! var.enable_istio_config
       #auth = "AUTH_MUTUAL_TLS"
-    }
+    }*/
   }
 
   network_policy {
     provider = "CALICO"
-    enabled  = "${var.network_policy_enabled}"
+    enabled  = var.enable_network_policy
   }
 
-  # Disable cert-based + static username based auth
   master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
+    username = var.basic_auth_username
+    password = var.basic_auth_password
 
-    username = ""
-    password = ""
+    client_certificate_config {
+      issue_client_certificate = var.enable_kubernetes_dashboard
+    }
   }
 
   timeouts {
     update = "20m"
     delete = "20m"
   }
-}
 
-resource "google_container_node_pool" "node_pool" {
-  provider   = "google-beta"
-  name       = "${var.name}"
-  cluster    = "${google_container_cluster.cluster.name}"
-  location   = "${var.location}"
-  node_count = "${var.node_count}"
-
-  node_config {
-    machine_type    = "${var.machine_type}"
-    service_account = "${var.service_account}"
-    tags            = ["${var.network_tags}"]
-    oauth_scopes    = ["${var.oauth_scopes}"]
-    labels          = "${var.node_labels}"
-
-    workload_metadata_config {
-      node_metadata = "${var.node_metadata}"
+  maintenance_policy {
+    daily_maintenance_window {
+      start_time = var.maintenance_start_time
     }
   }
+}
+
+# Prepare locals to keep the code cleaner
+
+locals {
+  latest_version     = data.google_container_engine_versions.location.latest_node_version
+  kubernetes_version = var.kubernetes_version != "latest" ? var.kubernetes_version : local.latest_version
+}
+
+# Pull in data
+# Get available master versions in our location to determine the latest version
+
+data "google_container_engine_versions" "location" {
+  location = var.location
+  project  = var.project
 }
--- gcp/gke/outputs.tf
+++ gcp/gke/outputs.tf
@@ -13,33 +13,36 @@
 # limitations under the License.
 
 output "name" {
-  value = "${google_container_cluster.cluster.name}"
-}
+  # This may seem redundant with the `name` input, but it serves an important
+  # purpose. Terraform won't establish a dependency graph without this to interpolate on.
+  description = "The name of the cluster master. This output is used for interpolation with node pools, other modules."
 
-output "endpoint" {
-  value = "${google_container_cluster.cluster.endpoint}"
+  value = google_container_cluster.cluster.name
 }
 
 output "master_version" {
-  value = "${google_container_cluster.cluster.master_version}"
+  description = "The Kubernetes master version."
+  value       = google_container_cluster.cluster.master_version
 }
 
-/*output "client_certificate" {
-  value = "${google_container_cluster.cluster.master_auth.0.client_certificate}"
+output "endpoint" {
+  description = "The IP address of the cluster master."
+  sensitive   = true
+  value       = google_container_cluster.cluster.endpoint
+}
+
+# The following outputs allow authentication and connectivity to the GKE Cluster.
+output "client_certificate" {
+  description = "Public certificate used by clients to authenticate to the cluster endpoint."
+  value       = base64decode(google_container_cluster.cluster.master_auth[0].client_certificate)
 }
 
 output "client_key" {
-  value = "${google_container_cluster.cluster.master_auth.0.client_key}"
-}*/
+  description = "Private key used by clients to authenticate to the cluster endpoint."
+  value       = base64decode(google_container_cluster.cluster.master_auth[0].client_key)
+}
 
 output "cluster_ca_certificate" {
-  value = "${google_container_cluster.cluster.master_auth.0.cluster_ca_certificate}"
-}
-
-output "unique_id" {
-  value = "${substr(md5(google_container_cluster.cluster.endpoint), 0, 8)}"
-}
-
-output "region" {
-  value = "${google_container_cluster.cluster.region}"
+  description = "The public certificate that is the root of trust for the cluster."
+  value       = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
 }
